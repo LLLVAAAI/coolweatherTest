19cb61a385e3fb864ba7b4cfa7b67e76f38173e5
Monkey <lvhao5515@mails.jlu.edu.cn>
2019-05-06 15:39:16 +0800

    引入必应每日一图

---
+++ app/src/main/java/com/coolweathertest1/android/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.coolweathertest1.android.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
--- app/src/main/java/com/coolweathertest1/android/util/Utility.java
+++ app/src/main/java/com/coolweathertest1/android/util/Utility.java
@@ -6,6 +6,7 @@ import com.coolweathertest1.android.db.City;
 import com.coolweathertest1.android.db.County;
 import com.coolweathertest1.android.db.Province;
 
+import com.coolweathertest1.android.gson.Weather;
 import com.google.gson.Gson;
 
 import org.json.JSONArray;
@@ -82,6 +83,19 @@ public class Utility {
         return false;
     }
 
-
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
